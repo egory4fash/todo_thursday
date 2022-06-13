2dfb56e8339233b716c120bae30c132c1d5c7b46
egory4fash <egormaksul@gmail.com>
2022-06-13 22:24:39 +0300

    13 lesson 2^26

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+
+export default {
+    title: 'API'
+}
+
+const settings = {
+    withCredentials: true,
+    headers: {
+        "API-KEY" : "4c1f8e2e-911c-4c20-9e00-c61208faf7aa"
+    }
+}
+
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+            .then((res) => {
+                setState(res.data);
+            })
+
+    }, [])
+
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists',{title:'smth'},settings)
+            .then( (res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${'9f70316b-26ca-44a8-b3f7-6df56dd04dc4'}`,settings)
+            .then( (res) => {
+                setState(res.data)
+            })
+    }, [])
+
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
